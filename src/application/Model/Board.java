450527aa46c7e16114134b5e9162e05d754d49ee
David Ryan Jackson <daverj65716@gmail.com>
2020-11-16 17:31:16 -0600

    Update Board.java
    Defining board class to store the pieces.

--- src/application/Model/Board.java
+++ src/application/Model/Board.java
@@ -1,5 +1,14 @@
 package application.Model;
 
+/*
+ * Stores all the information of pieces on the board
+ */
 public class Board {
-
+	int player1[];    //Stores the locations of player 1 pieces on the board
+	int player2[];    //Stores the location of player 2 pieces on the board
+	int Board[];      //Stores the special characters of the board
+	int pieceHolder1; //Stores how many pieces player 1 still has
+	int pieceHolder2; //Stores how many pieces player 2 still has
+	int pieceOff1;    //Stores the pieces the have mad it off the board for player 1 
+	int pieceoff2;    //Stores the pieces the have mad it off the board for player 1 
 }
