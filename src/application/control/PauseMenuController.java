aa925b47a8d66ac807db7ec908c31a481d7c7ba2
loganmpoole <logan.poole@protonmail.com>
2020-11-18 15:04:34 -0600

    more work on saving and loading

--- src/application/control/PauseMenuController.java
+++ src/application/control/PauseMenuController.java
@@ -7,6 +7,7 @@
 package application.control;
 
 import application.Main;
+import application.model.SaveFile;
 import javafx.application.Platform;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -25,21 +26,38 @@ public class PauseMenuController implements EventHandler<MouseEvent> {
 	public void handle( MouseEvent event ) {
 		String source = ((Button) event.getSource()).getId();
 		
+		/* Check which button was pressed */
 		switch ( source ) {
 		case "resumeGameButton":
 			System.out.print("PauseMenu - Resume Game button pressed.\n"); //sponge
 			Main.changeView(Main.prevState);
 			break;
+			
 		case "saveAndQuitButton":
 			System.out.print("PauseMenu - Save and Quit button pressed.\n"); //sponge
+			if (SaveFile.saveGame() < 0) {
+				errorLabel.setText("Error saving game.");
+				break;
+			}
+			
+			errorLabel.setText("Game successfully saved.");
+			Platform.exit();
 			break;
+			
 		case "saveButton":
 			System.out.print("PauseMenu - Save button pressed.\n"); //sponge
+			if (SaveFile.saveGame() < 0) {
+				errorLabel.setText("Error saving game.");
+			}
+			
+			errorLabel.setText("Game successfully saved.");
 			break;
+			
 		case "quitButton":
 			System.out.print("PauseMenu - Quit button pressed.\n"); //sponge
 			Platform.exit();
 			break;
+			
 		default:
 			break;
 		}
--- src/application/model/SaveFile.java
+++ src/application/model/SaveFile.java
@@ -1,19 +1,23 @@
 package application.model;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 import application.Main;
 
 /**
- * MVC model component for loading a gamestate from a text file
+ * MVC model component for handling saving and loading the game state
+ * to a text file
  */
-public class LoadGame {
+public class SaveFile {
 	
 	/**
-	 * Attempts to load a gamestate from save.txt
+	 * Attempts to load a game state from save.txt
 	 * @return 0 if successful, -1 otherwise
 	 */
 	public static int loadGame() {
@@ -55,6 +59,44 @@ public class LoadGame {
 		/* Change the view after reading the entire file */
 		Main.changeView(gameState);
 		
+		return 0;
+	}
+	
+	public static int saveGame() {
+		BufferedWriter bw;
+		File saveFile = new File("resources/save.txt");
+		FileWriter fw;
+		
+		/* Check if save file already exists */
+		if(!saveFile.exists()) {
+			try {
+				
+				/* Create a save file if one doesn't exist */
+				saveFile.createNewFile();
+			} catch (IOException e) {
+				
+				/* Couldn't create a save file */
+				e.printStackTrace();
+				return -1;
+			}
+		}
+		
+		/* 
+		 * Save file exists at this point:
+		 * write gamestate to save file
+		 */
+		try {
+			fw = new FileWriter(saveFile, false);
+			bw = new BufferedWriter(fw);
+			
+			bw.write(Main.prevState.toString());
+			
+			bw.close();
+			fw.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return 0;
 	}
 }
