983383d3e42fbc22bddac7e3c404c0c843ce3954
dcj657 <72472169+dcj657@users.noreply.github.com>
2020-11-28 20:45:31 -0600

    Update Dice.java

--- src/application/model/Dice.java
+++ src/application/model/Dice.java
@@ -1,5 +1,19 @@
 package application.model;
-
+import java.util.Random;
 public class Dice {
-
+  public int[] DiceArray(){
+    Random rand=new Random();
+    int[] die=new int[4];
+    for(int i=0;i<4;i++){
+      die[i]=rand.nextInt(2);
+    }
+    return die;
+  }
+  public int total(int die[]){
+    int sum=0;
+    for(int i=0;i<4;i++){
+      sum+=die[i];
+    }
+    return sum;
+  }
 }
