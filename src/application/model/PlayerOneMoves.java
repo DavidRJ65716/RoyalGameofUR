46250fddf0fdc37028bff81a05e46e8a39a61b8a
David Ryan Jackson <daverj65716@gmail.com>
2020-11-29 21:05:22 -0600

    Updating files
    Replacing files GameEngine, PlayeroneMoves, and PlayerTwoMoves with updated files

--- src/application/model/PlayerOneMoves.java
+++ src/application/model/PlayerOneMoves.java
@@ -6,7 +6,7 @@ package application.model;
 public class PlayerOneMoves {
 	
 	/*
-	 * Moves pieces on the board
+	 * Moves pieces already on the board
 	 * 
 	 * @Player player1
 	 * @Player player2
@@ -15,44 +15,65 @@ public class PlayerOneMoves {
 	 * @int dice@
 	 * Board board
 	 */
-	public static void MovePiece(Player player1, Player player2, int x, int y, Dice dice, Board board) {
+	public static void MovePiece(Player player1, Player player2, int x, int y, Dice dices, Board board) {
 		
 		int loc = cellConverter(x, y);
-		int newLoc = loc + dice.total;
+		int newLoc = loc + dices.total;
 		
-		
-		if ((player2.getPlayerB(newLoc) == 0) && (board.getBoard(newLoc) == 0)
-					&& (player1.getPlayerB(newLoc) == 0) && (player1.getPlayerB(loc) == 1)) {// No second player no event
+		if (loc != -1) {
+			if ((newLoc < 14) && (player2.getPlayerB(newLoc) == 0) && (board.getBoard(newLoc) == 0)
+					&& (player1.getPlayerB(newLoc) == 0) && (player1.getPlayerB(loc) == 1)
+						) {// No second player no event
+				
 			
-			player1.setPlayerB(loc, newLoc, 1);
-			player1.inplay = false;
-			player2.inplay = true;
-		} else if ((board.getBoard(newLoc) == 1)&& (player1.getPlayerB(newLoc) == 0) 
-					&& (player1.getPlayerB(loc) == 1)) {// No second player first event
+				player1.setPlayerB(loc, newLoc, 1);
+				player1.inplay = false;
+				player2.inplay = true;
+				dices.reset();
+			} else if ((newLoc < 14) && (board.getBoard(newLoc) == 1)&& (player1.getPlayerB(newLoc) == 0) 
+						&& (player1.getPlayerB(loc) == 1)) {// No second player first event
 			
-			player1.setPlayerB(loc, newLoc, 1);
-		} else if ((player2.getPlayerB(newLoc) == 2) && (board.getBoard(newLoc) == 0)
-				&& (player1.getPlayerB(newLoc) == 0) && (player1.getPlayerB(loc) == 1)) {// Second player no event
-			
-			player1.setPlayerB(loc, newLoc, 1);
-			player2.setPlayerB(newLoc, newLoc, 0);
-			player2.pieces += 1;
-			player1.inplay = false;
-			player2.inplay = true;
-		} else if ((player2.getPlayerB(newLoc) == 2) && (board.getBoard(newLoc) == 0)
-			&& (player1.getPlayerB(newLoc) == 0) && (player1.getPlayerB(loc) == 1)) {// second player second event
-			
-			player1.setPlayerB(loc, newLoc, 1);
-			player2.setPlayerB(newLoc, newLoc, 0);
-			player1.inplay = false;
-			player2.inplay = true;
-			dice.reset();
-		} 
-		
+				player1.setPlayerB(loc, newLoc, 1);
+				dices.reset();
+			} else if ((newLoc < 14) && (player2.getPlayerB(newLoc) == 2) && (board.getBoard(newLoc) != 2)
+					&& (player1.getPlayerB(newLoc) == 0) && (player1.getPlayerB(loc) == 1)
+						) {// Second player covers for second event
+				
+				player1.setPlayerB(loc, newLoc, 1);
+				player2.setPlayerB(newLoc, newLoc, 0);
+				player2.pieces++;
+				player1.inplay = false;
+				player2.inplay = true;
+				dices.reset();
+			} else if ((newLoc < 14) && (player2.getPlayerB(newLoc) != 2) && (board.getBoard(newLoc) == 2)
+					&& (player1.getPlayerB(newLoc) == 0) && (player1.getPlayerB(loc) == 1)
+						) {// no second player second event
+				
+				player1.setPlayerB(loc, newLoc, 1);
+			} else if ((newLoc < 14) && (board.getBoard(newLoc) == 3) && (player1.getPlayerB(newLoc) == 0)
+					&& (player1.getPlayerB(loc) == 1)) {// no second player third event
+				
+				player1.setPlayerB(loc, newLoc, 1);
+				dices.reset();
+			} else if (newLoc >= 14) {
+				if ((newLoc - 13) == 1) {
+					
+					player1.setPlayerB(loc, loc, 0);
+					player1.offpiecese++;
+					player1.inplay = false;
+					player2.inplay = true;
+					dices.reset();
+				}else {
+					
+					player1.inplay = false;
+					player2.inplay = true;
+				}
+			}
+		}
 	}
 	
 	/*
-	 * Moves pieces on the board
+	 * Adds new pieces to the board
 	 * 
 	 * @Player player1
 	 * @Player player2
@@ -70,10 +91,11 @@ public class PlayerOneMoves {
 			player1.setPlayerB(Loc, Loc, 1);
 			player1.inplay = false;
 			player2.inplay = true;
-			player1.pieces -= 1;
+			player1.pieces--;
 			dices.reset();
 		} else if ((board.getBoard(Loc) == 1) && (player1.getPlayerB(Loc) == 0) && (player1.pieces > 0)) { // Checks for piece first event
 			player1.setPlayerB(Loc, Loc, 1);
+			player1.pieces--;
 			dices.reset();
 		}
 	}
@@ -89,16 +111,27 @@ public class PlayerOneMoves {
 		
 		int loc = -1;
 		
-		switch (y) {
-		case 0:
-			loc = (x - 3) * (-1);
-			break;
-		case 1:
+		if (y == 0) {
+			
+			if(x <=3) {
+				
+				loc = (x - 3) * (-1);
+			} else {
+				
+				if(x == 7){
+					
+					loc = x + 5;
+				} else {
+					
+					loc = x + 7;
+				}
+			}
+		} else if (y == 1) {
+			
 			loc = x + 4;
-			break;
-		default:
+			
+		} else {
 			loc = -1;
-			break;			
 		}
 		
 		return loc;
--- src/application/model/PlayerTwoMoves.java
+++ src/application/model/PlayerTwoMoves.java
@@ -1,14 +1,102 @@
 package application.model;
-
+/**
+ * Moves pieces for player two 
+ */
 public class PlayerTwoMoves {
 	
-	
-	public static void MovePiece(Player player1, Player player2, int x, int y, Dice dice, Board board) {
+	/*
+	 * Moves pieces already on the board
+	 * 
+	 * @Player player1
+	 * @Player player2
+	 * @int x
+	 * @int y
+	 * @int dice@
+	 * Board board
+	 */
+	public static void MovePiece(Player player1, Player player2, int x, int y, Dice dices, Board board) {
+		
+		int loc = cellConverter(x, y);
+		int newLoc = loc + dices.total;
+		
+		if (loc != -1) {
+			if ((newLoc < 14) && (player2.getPlayerB(newLoc) == 0) && (board.getBoard(newLoc) == 0)
+					&& (player1.getPlayerB(newLoc) == 0) && (player2.getPlayerB(loc) == 2)) {// No second player no event
+			
+				player2.setPlayerB(loc, newLoc, 2);
+				player1.inplay = true;
+				player2.inplay = false;
+				dices.reset();
+			} else if ((newLoc < 14) && (board.getBoard(newLoc) == 1) && (player2.getPlayerB(newLoc) == 0) 
+						&& (player2.getPlayerB(loc) == 2)) {// No first player first event
+			
+				player2.setPlayerB(loc, newLoc, 2);
+				dices.reset();
+			} else if ((newLoc < 14) && (player1.getPlayerB(newLoc) == 1) && (board.getBoard(newLoc) != 2)
+					&& (player2.getPlayerB(newLoc) == 0) && (player2.getPlayerB(loc) == 2)
+						) {// first player covers for second event
+			
+				player2.setPlayerB(loc, newLoc, 2);
+				player1.setPlayerB(newLoc, newLoc, 0);
+				player1.pieces++;
+				player2.inplay = false;
+				player1.inplay = true;
+				dices.reset();
+			} else if ((newLoc < 14) && (player2.getPlayerB(newLoc) != 2) && (board.getBoard(newLoc) == 2)
+					&& (player1.getPlayerB(newLoc) == 0) && (player1.getPlayerB(loc) == 1)
+						) {// no second player second event
+				
+				player2.setPlayerB(loc, newLoc, 2);
+				dices.reset();
+			} else if ((newLoc < 14) && (board.getBoard(newLoc) == 3) && (player1.getPlayerB(newLoc) == 0) 
+					&& (player1.getPlayerB(loc) == 1)) {// no second player third event
+				
+				player2.setPlayerB(loc, newLoc, 2);
+				dices.reset();
+			} else if (newLoc >= 14) {
+				if ((newLoc - 13) == 1) {
+					
+					player2.setPlayerB(loc, loc, 0);
+					player2.offpiecese++;
+					player2.inplay = false;
+					player1.inplay = true;
+					dices.reset();
+				} else {
+					
+					player2.inplay = false;
+					player1.inplay = true;
+				}
+				
+			}
+		}
 		
 	}
 	
-	public static void NewPiece(Player player1, Player player2, Dice dice, Board board) {
+	/*
+	 * Add pieces to the board 
+	 * 
+	 * @Player player1
+	 * @Player player2
+	 * @int x
+	 * @int y
+	 * @int dice@
+	 * Board board
+	 */
+	public static void NewPiece(Player player1, Player player2, Dice dices, Board board) {
 		
+		int Loc = dices.total - 1;		
+		
+		if ((board.getBoard(Loc) == 0) && (player2.getPlayerB(Loc) == 0) && (player2.pieces > 0)) {// Checks for piece no event
+			player2.setPlayerB(Loc, Loc, 2);
+			player2.inplay = false;
+			player1.inplay = true;
+			player2.pieces -= 1;
+			dices.reset();
+		} else if ((board.getBoard(Loc) == 1) && (player2.getPlayerB(Loc) == 0) && (player2.pieces > 0)) { // Checks for piece first event
+			player2.setPlayerB(Loc, Loc, 2);
+			player2.pieces -= 1;
+			dices.reset();
+		}
 	}
 	/*
 	 * Converts coordinates to work with an array
@@ -19,15 +107,28 @@ public class PlayerTwoMoves {
 	public static int cellConverter(int x, int y) {
 		
 		int loc = -1;
-		
-		switch (y) {
-		case 0:
-			loc = (x - 3) * (-1);
-			break;
-		case 1:
-			loc = x + 4;
-			break;
+				
+		if (y == 2) {
 			
+			if(x <=3) {
+				
+				loc = (x - 3) * (-1);
+			} else {
+				
+				if(x == 7){
+					
+					loc = x + 5;
+				} else {
+					
+					loc = x + 7;
+				}
+			}
+		} else if (y == 1) {
+			
+			loc = x + 4;
+		} else {
+			
+			loc = -1;
 		}
 		
 		return loc;
